7a0e8fe8991fd4c5d6b4da716abbe3b4ba9f880a
ImaDoofus <ImaDoofusyt@gmail.com>
2022-05-28 13:35:21 -0600

    update give_item.js comments

--- actions/loaders/give_item.js
+++ actions/loaders/give_item.js
@@ -5,7 +5,7 @@ export default (actionData) => {
 	if (actionData.item) {
 		sequence.push({ type: 'guiClick', slot: 10 });
 		sequence.push({ type: 'loadItem', item: actionData.item, slot: 36 }); // slot 36 is the first slot in the hotbar
-		sequence.push({ type: 'guiClick', slot: 63 }); // slot 63 is the first slot in the inventory when "Edit Actions" gui is opened
+		sequence.push({ type: 'guiClick', slot: 63 }); // slot 63 is the first slot in the inventory when "Select an Item" gui is opened
 	}
 
 	if (actionData.allowMultiple) {
