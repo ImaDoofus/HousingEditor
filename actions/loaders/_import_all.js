2014eae3fabf6e60e699caa781bcd2fc5937b930
ImaDoofus <imadoofusyt@gmail.com>
2023-11-10 18:44:04 -0700

    merge

---
+++ actions/loaders/_import_all.js
@@ -0,0 +1,77 @@
+import conditional from "./conditional";
+import change_player_group from "./change_player_group";
+import kill_player from "./kill_player";
+import full_heal from "./full_heal";
+import go_to_house_spawn from "./go_to_house_spawn";
+import display_title from "./display_title";
+import display_action_bar from "./display_action_bar";
+import reset_inventory from "./reset_inventory";
+import change_max_health from "./change_max_health";
+import parkour_checkpoint from "./parkour_checkpoint";
+import give_item from "./give_item";
+import remove_item from "./remove_item";
+import send_a_chat_message from "./send_a_chat_message";
+import apply_potion_effect from "./apply_potion_effect";
+import clear_all_potion_effects from "./clear_all_potion_effects";
+import give_experience_levels from "./give_experience_levels";
+import send_to_lobby from "./send_to_lobby";
+import change_player_stat from "./change_player_stat";
+import change_global_stat from "./change_global_stat";
+import change_team_stat from "./change_team_stat";
+import teleport_player from "./teleport_player";
+import fail_parkour from "./fail_parkour";
+import play_sound from "./play_sound";
+import set_compass_target from "./set_compass_target";
+import set_gamemode from "./set_gamemode";
+import change_health from "./change_health";
+import change_hunger_level from "./change_hunger_level";
+import random_action from "./random_action";
+import use_remove_held_item from "./use_remove_held_item";
+import apply_inventory_layout from "./apply_inventory_layout";
+import trigger_function from "./trigger_function";
+import enchant_held_item from "./enchant_held_item";
+import set_player_team from "./set_player_team";
+import pause_execution from "./pause_execution";
+import display_menu from "./display_menu";
+import close_menu from "./close_menu";
+import balance_player_team from "./balance_player_team";
+
+export default {
+  conditional,
+  change_player_group,
+  kill_player,
+  full_heal,
+  go_to_house_spawn,
+  display_title,
+  display_action_bar,
+  reset_inventory,
+  change_max_health,
+  parkour_checkpoint,
+  give_item,
+  remove_item,
+  send_a_chat_message,
+  apply_potion_effect,
+  clear_all_potion_effects,
+  give_experience_levels,
+  send_to_lobby,
+  change_player_stat,
+  change_global_stat,
+  teleport_player,
+  fail_parkour,
+  play_sound,
+  set_compass_target,
+  set_gamemode,
+  change_health,
+  change_hunger_level,
+  random_action,
+  use_remove_held_item,
+  apply_inventory_layout,
+  trigger_function,
+  enchant_held_item,
+  change_team_stat,
+  set_player_team,
+  pause_execution,
+  display_menu,
+  close_menu,
+  balance_player_team,
+};
