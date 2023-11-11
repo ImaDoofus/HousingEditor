0199688bcd46a2bf55d35ce9bbc52fb02f124af5
ImaDoofus <imadoofusyt@gmail.com>
2023-11-10 18:24:41 -0700

    cache npc list
    Co-authored-by: Sin_ender <sinender@users.noreply.github.com>
    Co-authored-by: VariousCacti <VariousCacti@users.noreply.github.com>
    Co-authored-by: Arisings <Arisings@users.noreply.github.com>

--- api/hostname.js
+++ api/hostname.js
@@ -1,3 +1,3 @@
-const isProduction = false;
+const isProduction = true;
 
 export const HOSTNAME = isProduction ? "https://api.housingeditor.com" : "http://localhost:3000";
--- commands/npcListCommand.js
+++ commands/npcListCommand.js
@@ -1,42 +1,63 @@
+const EntityArmorStand = Java.type("net.minecraft.entity.item.EntityArmorStand");
+const EntityOtherPlayerMP = Java.type("net.minecraft.client.entity.EntityOtherPlayerMP");
+
+let cachedNPCs = {};
+let cachedArmorstands = {};
+
 register("command", () => {
   if (!TabList.getFooter().includes("You are in ")) return ChatLib.chat("&cYou must be in a housing to use this command!");
-  let npcs = [];
 
-  // Collect all NPCs in your world as objects
-  World.getAllPlayers().forEach((p) => {
-    if (p.getUUID().version() === 2 && p.getName() !== "Carpenter ") {
-      npcs.push(p);
-    }
-  });
+  let npcs = Object.values(cachedNPCs);
+  let armorstands = Object.values(cachedArmorstands);
+  if (npcs.length === 0) return ChatLib.chat("&cNo NPCs were found!");
+  let output = new Message(`&aNPC List &7(${npcs.length})\n`);
 
-  if (npcs.length === 0) return ChatLib.chat("&cThere are no NPCs in your render distance!");
-  let output = new Message(`&7NPC List &b(${npcs.length})\n`);
-
-  // Iterate through the NPC list and add an entry in the chat message that will be sent listing all the NPCs
   for (let i = 0; i < npcs.length; i++) {
     let npc = npcs[i];
-    output.addTextComponent(new TextComponent(`&a${i + 1} &7- `)); // index of NPC
-    let name = getNPCDisplayName(npc) || "&cNo Name";
-    output.addTextComponent(
-      new TextComponent(`&r${name}`).setHoverValue(`&eDisplay Name: &r${name}\n&eEntity Name: &7${npc.getName()}\n&eUUID: &7${npc.getUUID()}`)
-    );
-    output.addTextComponent(new TextComponent(`&7: `));
-    output.addTextComponent(
-      new TextComponent(`&b${npc.getX()}&3, &b${npc.getY()}&3, &b${npc.getZ()}`)
+    // Nametag is the armor stand 0.09375 blocks away from NPC
+    let name = armorstands.find((e) => e.distanceTo(npc) === 0.09375)?.name || "&cUnknown";
+
+    [
+      new TextComponent(`  &a${i + 1} &7- `), // index of NPC
+      new TextComponent(`&r${name}`)
+        .setHoverValue(`&eDisplay Name: &r${name}\n&eEntity Name: &7${npc.name}\n&eUUID: &7${npc.uuid}`)
+        .setClick("suggest_command", ChatLib.removeFormatting([name, npc.name, npc.uuid].join(" "))),
+      new TextComponent(`&7: `),
+      new TextComponent(`&b${npc.x}&3, &b${npc.y}&3, &b${npc.z}`)
         .setHoverValue(`&eClick to teleport to NPC`)
-        .setClick("run_command", `/tp ${npc.getX()} ${npc.getY()} ${npc.getZ()}`)
-    ); // NPC's coordinates
-    if (i < npcs.length - 1) output.addTextComponent(`\n`);
+        .setClick("run_command", `/tp ${npc.x} ${npc.y} ${npc.z}`),
+      new TextComponent(`\n`),
+    ].forEach((c) => output.addTextComponent(c));
   }
   ChatLib.chat(output);
 })
   .setName("npclist")
   .setAliases(["npcl"]);
 
-// NPC's name is separate hologram so this function will try to get a NPC's display name
-const EntityArmorStand = Java.type("net.minecraft.entity.item.EntityArmorStand");
-function getNPCDisplayName(npc) {
-  const isSeperateHologram = (e) => e.distanceTo(npc) === 0.09375;
+register("step", () => {
   let armorStands = World.getAllEntitiesOfType(EntityArmorStand.class);
-  return armorStands.find(isSeperateHologram)?.getName();
+  armorStands.forEach((armorStand) => (cachedArmorstands[armorStand.getUUID()] = new CachedEntity(armorStand)));
+
+  let npcs = World.getAllEntitiesOfType(EntityOtherPlayerMP.class);
+  npcs = npcs.filter((p) => p.getUUID().version() === 2 && p.getName() !== "Carpenter ");
+  npcs.forEach((npc) => (cachedNPCs[npc.getUUID()] = new CachedEntity(npc)));
+}).setFps(1);
+
+register("worldLoad", () => {
+  cachedArmorstands = {};
+  cachedNPCs = {};
+});
+
+class CachedEntity {
+  constructor(entity) {
+    this.x = entity.getX();
+    this.y = entity.getY();
+    this.z = entity.getZ();
+    this.name = entity.getName();
+    this.uuid = entity.getUUID();
+  }
+
+  distanceTo(other) {
+    return Math.sqrt((this.x - other.x) ** 2 + (this.y - other.y) ** 2 + (this.z - other.z) ** 2);
+  }
 }
